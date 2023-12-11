2ab07990bd271cd61644da6a21a3c1202409c0d2
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-12-11 10:15:14 +0000

    Create MRE

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {},
   },
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,17 @@
-/// <reference types="cypress" />
+const filename = 'example.json'
+
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('works once', () => {
+    cy.visit('/')
+    cy.fixture(filename).as('file')
+    cy.get('input[type="file"]').selectFile('@file')
+    cy.get('#filename').should('contain.text', filename)
+  })
+
+  it('fails the second time', () => {
+    cy.visit('/')
+    cy.fixture(filename).as('file')
+    cy.get('input[type="file"]').selectFile('@file')
+    cy.get('#filename').should('contain.text', filename)
   })
 })
